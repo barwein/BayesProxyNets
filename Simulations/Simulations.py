6438176c1575110bbc55619dc43a30bb80dbf756
Warik21 <eriklager@mail.tau.ac.il>
2024-03-27 20:20:38 +0200

    Update simulations file

--- Simulations/Simulations.py
+++ Simulations/Simulations.py
@@ -2,22 +2,23 @@
 import time
 import numpy as np
 import pandas as pd
-import multiprocessing
-import os
+# import multiprocessing
+# import os
 # import jax
-import jax.numpy as jnp
+# import jax.numpy as jnp
 from jax import random
-from jax.scipy.special import expit
-import numpyro.distributions as dist
-import numpyro
-from numpyro.contrib.funsor import config_enumerate
-# from tqdm import tqdm
-from joblib import Parallel, delayed
-from numpyro.infer import MCMC, NUTS, Predictive
+# from jax.scipy.special import expit
+# import numpyro.distributions as dist
+# import numpyro
+# from numpyro.contrib.funsor import config_enumerate
+# # from tqdm import tqdm
+# from joblib import Parallel, delayed
+# from numpyro.infer import MCMC, NUTS, Predictive
 
 from src.Aux_functions import DataGeneration, Outcome_MCMC, Network_MCMC, Bayes_Modular, create_noisy_network
 
-def One_simuation_iter(n, theta, gamma, eta, sig_y, pz, iter):
+
+def one_simuation_iter(n, theta, gamma, eta, sig_y, pz, iter):
     rng_key = random.PRNGKey(iter)
     rng_key, rng_key_ = random.split(rng_key)
     # Get data
@@ -52,12 +53,12 @@ def One_simuation_iter(n, theta, gamma, eta, sig_y, pz, iter):
     # with 2S we need the `network_pred_mean_post`, and with the others the `network_pred` object
     # 2S
     cut_2S_mcmc = Bayes_Modular(data=df_obs, n=n, bm_type="cut-2S",
-                                post_predictive=network_pred_mean_post, n_rep=20, iter=iter)
+                                post_predictive=network_pred_mean_post, n_rep=10, iter=iter)
     cut_2S_mcmc.run_inference()
     cut_2S_results = cut_2S_mcmc.get_results()
     # 3S
     cut_3S_mcmc = Bayes_Modular(data=df_obs, n=n, bm_type="cut-3S",
-                                post_predictive=network_pred, n_rep=20, iter=iter)
+                                post_predictive=network_pred, n_rep=10, iter=iter)
     cut_3S_mcmc.run_inference()
     cut_3S_results = cut_3S_mcmc.get_results()
     # plugin
@@ -65,38 +66,37 @@ def One_simuation_iter(n, theta, gamma, eta, sig_y, pz, iter):
                                 post_predictive=network_pred, iter=iter)
     plugin_mcmc.run_inference()
     plugin_results = plugin_mcmc.get_results()
+
+    # Combine all
     results_all = pd.concat([oracle_results, obs_results, cut_2S_results, cut_3S_results, plugin_results])
     results_all['iter'] = iter
     return results_all
 
+
 if __name__ == "__main__":
-    os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
+    # os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
     RANDOM_SEED = 892357143
     rng = np.random.default_rng(RANDOM_SEED)
 
-
-    N = 30
     THETA = [-2, -0.5]
     GAMMA = [0.05, 0.25]
     ETA = [-1, -3, 0.5, -0.25]
     SIG_Y = 1
     PZ = 0.3
     BM_TYPES = ["cut-2S", "cut-3S", "plugin"]
-    N_SIM = 200
+    N = 300
+    N_SIM = 30
 
-    rng_key = random.PRNGKey(0)
-    rng_key, rng_key_ = random.split(rng_key)
-
-
-    print(os.getcwd())
+    start = time.time()
     twosims = pd.DataFrame()
-    for iter in range(2):
-        curr_result = One_simuation_iter(n=N, theta=THETA, gamma=GAMMA, eta=ETA,
+    for iter in range(N_SIM):
+        curr_result = one_simuation_iter(n=N, theta=THETA, gamma=GAMMA, eta=ETA,
                                          sig_y=SIG_Y, pz=PZ, iter=iter)
+        with_header = iter == 0
         curr_result.to_csv("linear_dgp_noisy_network_N300.csv",
                            mode='a',
-                           index=False, header=True)
+                           index=False, header=with_header)
         twosims = pd.concat([twosims,curr_result])
         print("Done iteration {}".format(iter))
 
-    print(twosims.to_string())
+    print("Elapsed time: ", time.time()-start)
--- src/Aux_functions.py
+++ src/Aux_functions.py
@@ -15,10 +15,11 @@ from numpyro.contrib.funsor import config_enumerate
 from joblib import Parallel, delayed
 from numpyro.infer import MCMC, NUTS, Predictive
 
-N_CORES = multiprocessing.cpu_count()
-os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=4"
+N_CORES = multiprocessing.cpu_count()-1
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
 RANDOM_SEED = 892357143
 rng = np.random.default_rng(RANDOM_SEED)
+
 
 class DataGeneration:
     def __init__(self, n, theta, eta, sig_y, pz):
@@ -63,7 +64,7 @@ class DataGeneration:
 
     def generate_outcome(self):
         mu_y = self.eta[0] + self.eta[1]*self.Z + self.eta[2]*self.exposures + self.eta[3]*self.X
-        return mu_y + rng.normal(loc=0,scale=self.sig_y,size=self.n)
+        return mu_y + rng.normal(loc=0, scale=self.sig_y, size=self.n)
 
     def get_data(self):
         return {"Z" : self.Z, "X" : self.X, "Y" : self.Y,
@@ -78,7 +79,7 @@ def create_noisy_network(adj_mat, gamma, n):
     for i in range(0, n):  # add noise
         for j in range(i + 1, n):
             if adj_mat[i, j] == 1:
-                obs_mat[i, j] = rng.binomial(n=1, p=1 - gamma[1], size=1)[0]  # retain existing edge w.p. `1-gamma1`
+                obs_mat[i, j] = rng.binomial(n=1, p=1-gamma[1], size=1)[0]  # retain existing edge w.p. `1-gamma1`
             else:
                 obs_mat[i, j] = rng.binomial(n=1, p=gamma[0], size=1)[0]  # add non-existing edge w.p. `gamma0`
     obs_mat = obs_mat + obs_mat.T
@@ -86,20 +87,25 @@ def create_noisy_network(adj_mat, gamma, n):
     return {"obs_mat" : obs_mat,
             "triu_obs" : triu_obs}
 
+
 def triu_to_mat(TRIU, n):
     AM = np.zeros((n,n))
     AM[np.triu_indices(n=n,k=1)] = TRIU
     return AM + AM.T
 
+
 def q025(x):
     return x.quantile(.025)
+
 
 def q975(x):
     return x.quantile(.975)
 
+
 def between_var(x, mean_all):
     n_rep = len(x)
     return (1/(n_rep - 1))*np.sum(np.square(x-mean_all))
+
 
 @config_enumerate
 def network_model(X_diff, TriU, n):
@@ -117,6 +123,7 @@ def network_model(X_diff, TriU, n):
         prob_misspec = triu_star * (1 - gamma1) + (1 - triu_star) * gamma0
         numpyro.sample("obs_triu", dist.Bernoulli(probs=prob_misspec), obs=TriU)
 
+
 def outcome_model(Y, Z, X, A, n):
     with numpyro.plate("eta_i", 4):
         eta = numpyro.sample("eta", dist.Normal(0, 10))
@@ -125,6 +132,7 @@ def outcome_model(Y, Z, X, A, n):
     mu_y = eta[0] + eta[1]*Z + eta[2]*expos + eta[3]*X
     with numpyro.plate("n", n):
         numpyro.sample("Y", dist.Normal(loc=mu_y, scale=sig), obs=Y)
+
 
 def outcome_stat_model(Y, Z, X, expos, n):
     with numpyro.plate("eta_i",4):
@@ -150,6 +158,7 @@ class Network_MCMC:
         kernel = NUTS(network_model)
         return MCMC(kernel, num_warmup=self.n_warmup, num_samples=self.n_samples,
                     num_chains=self.n_chains, progress_bar=False)
+
     def run_network_model(self):
         self.network_m.run(self.rng_key, X_diff=self.X_diff, TriU=self.triu, n=self.n)
 
@@ -165,6 +174,7 @@ class Network_MCMC:
             posterior_predictive = Predictive(model=network_model, posterior_samples=posterior_samples,
                                               infer_discrete=True)
             return posterior_predictive(self.rng_key, X_diff=self.X_diff, TriU=self.triu, n=self.n)["triu_star"]
+
 
 class Outcome_MCMC:
     def __init__(self, data, n, type, rng_key, iter, suff_stat = False, n_warmup=1000, n_samples=3000, n_chains=4):
@@ -191,6 +201,7 @@ class Outcome_MCMC:
 
         return MCMC(kernel, num_warmup=self.n_warmup, num_samples=self.n_samples,
                     num_chains=self.n_chains, progress_bar=False)
+
     def run_outcome_model(self):
         if not self.suff_stat:
             self.outcome_m.run(self.rng_key, Y=self.Y,Z=self.Z,X=self.X,A=self.adj_mat,n=self.n)
@@ -198,10 +209,6 @@ class Outcome_MCMC:
             self.outcome_m.run(self.rng_key, Y=self.Y,Z=self.Z,X=self.X,expos=self.exposures,n=self.n)
 
     def get_summary_outcome_model(self):
-        """
-
-        :rtype: object
-        """
         posterior_samples = self.outcome_m.get_samples()
         mean_posterior = np.mean(posterior_samples["eta"],axis=0)[2]
         median_posterior = np.median(posterior_samples["eta"],axis=0)[2]
@@ -220,9 +227,10 @@ class Outcome_MCMC:
                              'type' : self.type},
                             index = [self.iter])
 
+
 class Bayes_Modular:
     def __init__(self, data, n, bm_type, post_predictive, n_rep = 1000, n_warmup = 250,
-                 n_samples = 500, iter = 0, n_cores = N_CORES):
+                 n_samples=500, iter=0, n_cores=N_CORES):
         self.X = data["X"]
         self.Z = data["Z"]
         self.Y = data["Y"]
@@ -240,6 +248,7 @@ class Bayes_Modular:
         self.n_cores = n_cores
         self.Y_mcmc = self.MCMC_obj()
         self.results = None
+
     def MCMC_obj(self):
         return MCMC(NUTS(outcome_model), num_warmup=self.n_warmup, num_samples=self.n_samples,
                     num_chains=1, progress_bar=False)
@@ -247,7 +256,8 @@ class Bayes_Modular:
     def stage_aux(self, i):
         # sample network
         if self.type == "cut-2S":
-            curr_mat = self.post_predictive(random.PRNGKey(i**2),X_diff=self.X_diff,TriU=self.triu,n=self.n)
+            curr_mat = self.post_predictive(random.PRNGKey(i**2), X_diff=self.X_diff,
+                                            TriU=self.triu, n=self.n)
             curr_mat = triu_to_mat(curr_mat["triu_star"], self.n)
         elif self.type == "cut-3S":
             curr_mat = triu_to_mat(self.post_predictive[i,], self.n)
@@ -268,8 +278,8 @@ class Bayes_Modular:
         return pd.concat(twostage_post_samp, axis=0)
 
     def threestage_inference(self):
-        i_range = np.random.choice(a = range(self.post_predictive.shape[0]),
-                                   size = self.n_rep, replace = False)
+        i_range = np.random.choice(a=range(self.post_predictive.shape[0]),
+                                   size=self.n_rep, replace=False)
         threestage_post_samp = Parallel(n_jobs=self.n_cores)(delayed(self.stage_aux)(i) for i in i_range)
         return pd.concat(threestage_post_samp, axis=0)
 
@@ -329,78 +339,5 @@ class Bayes_Modular:
             return self.cut_posterior_summarized()
         if self.type in ["plugin"]:
             return self.plugin_posterior_summarized()
-
-
-if __name__ == "__main__":
-
-    N = 300
-    THETA = [-2, -0.5]
-    GAMMA = [0.05, 0.25]
-    ETA = [-1, -3, 0.5, -0.25]
-    SIG_Y = 1
-    PZ = 0.3
-    BM_TYPES = ["cut-2S", "cut-3S", "plugin"]
-
-    rng_key = random.PRNGKey(0)
-    rng_key, rng_key_ = random.split(rng_key)
-
-    # Get data
-    df_true = DataGeneration(n=N, theta=THETA, eta=ETA, sig_y=SIG_Y, pz=PZ).get_data()
-    # Generate noisy network measurement
-    obs_network = create_noisy_network(df_true["adj_mat"], GAMMA, N)
-    # save observed df and update A* and triu
-    df_obs = df_true.copy()
-    df_obs["adj_mat"] = obs_network["obs_mat"]
-    df_obs["triu"] = obs_network["triu_obs"]
-
-    # Run MCMC with true network (as given)
-    oracle_outcome_mcmc = Outcome_MCMC(data=df_true, n=N, type = "oracle", rng_key=rng_key, iter = 1)
-    oracle_outcome_mcmc.run_outcome_model()
-    oracle_results = oracle_outcome_mcmc.get_summary_outcome_model()
-    # Run MCMC with observed network (as given)
-    obs_outcome_mcmc = Outcome_MCMC(data=df_obs, n=N, type = "observed", rng_key=rng_key, iter = 1)
-    obs_outcome_mcmc.run_outcome_model()
-    obs_results = obs_outcome_mcmc.get_summary_outcome_model()
-    print(pd.concat([oracle_results, obs_results]))
-
-    print("Running network model")
-    # Run network module
-    network_mcmc = Network_MCMC(data=df_obs, n=N, rng_key=rng_key)
-    network_mcmc.run_network_model()
-    print("Done with netowk model! getting predictive A* | . ")
-    # get predictive distributions
-    network_pred = network_mcmc.get_network_predictive(mean_posterior=False)
-    network_pred_mean_post = network_mcmc.get_network_predictive(mean_posterior=True)
-
-    print("Running estimation with cut-posterior and plugin")
-    # Cut-posterior and plugin estimates
-    # with 2S we need the `network_pred_mean_post`, and with the others the `network_pred` object
-    # 2S
-    cut_2S_mcmc = Bayes_Modular(data=df_obs, n=N, bm_type="cut-2S",
-                                post_predictive=network_pred_mean_post, n_rep=20, iter=1)
-    cut_2S_mcmc.run_inference()
-    cut_2S_results = cut_2S_mcmc.get_results()
-    # 3S
-    cut_3S_mcmc = Bayes_Modular(data=df_obs, n=N, bm_type="cut-3S",
-                                post_predictive=network_pred, n_rep=20, iter=1)
-    cut_3S_mcmc.run_inference()
-    cut_3S_results = cut_3S_mcmc.get_results()
-    # plugin
-    plugin_mcmc = Bayes_Modular(data=df_obs, n=N, bm_type="plugin",
-                                post_predictive=network_pred, iter=1)
-    plugin_mcmc.run_inference()
-    plugin_results = plugin_mcmc.get_results()
-    print(pd.concat([cut_2S_results, cut_3S_results,plugin_results]))
-    print("ALL together:")
-    results_all = pd.concat([oracle_results,obs_results,cut_2S_results,cut_3S_results,plugin_results])
-    results_all['iter'] = 1
-    print(results_all)
-    print(results_all.to_string())
-
-
-
-    # TODO: add class of `cut-posterior` (two and three stages) and `plug-in`
-    # TODO: create simulation iterations procedure (in other py file perhaps)
-
 
 
