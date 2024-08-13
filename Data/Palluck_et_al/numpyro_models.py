b6d694c0b49870655a49b2fa301cda7e37580f7c
barwein <barwein@mail.tau.ac.il>
2024-08-13 16:52:26 +0300

    Modified HSGP model and analysis

--- Data/Palluck_et_al/numpyro_models.py
+++ Data/Palluck_et_al/numpyro_models.py
@@ -33,18 +33,25 @@ from hsgp.spectral_densities import diag_spectral_density_squared_exponential
 def noisy_networks_model(x_eq: jnp.ndarray, triu_v: jnp.ndarray, N: int, K = 2):
     # True network priors
     # Latent variable for each unit from bi-normal distribution
-    # sigma_sq = numpyro.sample("sigma_sq", dist.InverseGamma(1.0, 1.0))
+    sigma_sq = numpyro.sample("sigma_sq", dist.InverseGamma(0.1, 1.0))
     # cov_matrix = sigma_sq * jnp.eye(K)
     # cov_matrix = 5.0 * jnp.eye(K)
     # N = ((1 + jnp.sqrt(1 + 8*x_eq.shape[0]))/2).astype(int)
     # N = jax.lax.convert_element_type((1 + jnp.sqrt(1 + 8*x_eq.shape[0]))/2, jnp.int32)
-    # with numpyro.plate("nu_i", N):
-    #     nu = numpyro.sample("nu", dist.MultivariateNormal(loc=jnp.zeros(K), covariance_matrix=cov_matrix))
-        # nu_standard = numpyro.sample("nu_standard", dist.MultivariateNormal(loc=jnp.zeros(K), covariance_matrix=jnp.eye(K)))
+    with numpyro.plate("nu_i", N):
+        # nu = numpyro.sample("nu", dist.MultivariateNormal(loc=jnp.zeros(K), covariance_matrix=cov_matrix))
+        # nu_standard = numpyro.sample("nu_standard", dist.Normal(jnp.zeros(K), jnp.ones(K)))
+        nu_standard = numpyro.sample("nu_standard", dist.MultivariateNormal(loc=jnp.zeros(K), covariance_matrix=jnp.eye(K)))
         # nu = numpyro.deterministic("nu", nu_standard*jnp.sqrt(sigma_sq))
         # nu = numpyro.deterministic("nu", nu_standard*3.0)
 
+    # nu_standard = numpyro.sample("nu_standard", dist.Normal(0, 1).expand((N, K)))
+
+    nu = numpyro.deterministic("nu", nu_standard * jnp.sqrt(sigma_sq))
     # Calculate pairwise differences
+    idx = jnp.triu_indices(n=N, k=1)
+    nu_diff = nu[idx[0]] - nu[idx[1]]
+    nu_diff_norm_val = jnp.linalg.norm(nu_diff, axis=1)
     # nu_diff = nu[:, None, :] - nu[None, :, :]  # Shape: (N*(N-1)/2, N*(N-1)/2, K)
     # nu_diff_norm = jnp.linalg.norm(nu_diff, axis=2)  # Shape: (N*(N-1)/2, N*(N-1)/2)
     # nu_diff_norm_val = nu_diff_norm[jnp.triu_indices(n=N,k=1)] # Shape: (N*(N-1)/2,)
@@ -54,34 +61,55 @@ def noisy_networks_model(x_eq: jnp.ndarray, triu_v: jnp.ndarray, N: int, K = 2):
     theta_intercept = numpyro.sample("theta_intercept", dist.Normal(0, 5))
     with numpyro.plate("theta_i", x_eq.shape[1]):
         theta = numpyro.sample("theta", dist.Normal(0, 5))
+    # theta_intercept = numpyro.sample("theta_intercept", dist.Normal(0, 5))
+    # theta = numpyro.sample("theta", dist.Normal(0, 5).expand((x_eq.shape[1],)))
     # Save logits for A*
     # mu_net = theta_intercept + jnp.dot(x_eq, theta) + theta_latent*nu_diff_norm_val
-    # mu_net = theta_intercept + jnp.dot(x_eq, theta) - nu_diff_norm_val
-    mu_net = theta_intercept + jnp.dot(x_eq, theta)
+    mu_net = theta_intercept + jnp.dot(x_eq, theta) - nu_diff_norm_val
+    # mu_net = theta_intercept + jnp.dot(x_eq, theta)
 
     if triu_v.ndim == 1:
-        with numpyro.plate("gamma_i", 2):
+        with numpyro.plate("gamma_i", 3):
+        # # with numpyro.plate("gamma_i", 2):
             gamma = numpyro.sample("gamma", dist.Normal(0, 5))
+        # gamma = numpyro.sample("gamma", dist.Normal(0, 5).expand((3,)))
 
         with numpyro.plate("A* and A", x_eq.shape[0]):
-            triu_star = numpyro.sample("triu_star", dist.Bernoulli(logits=mu_net),
-                                       infer={"enumerate": "parallel"})
-            logit_misspec = triu_star*gamma[0] + (1 - triu_star)*gamma[1]
+            triu_star = numpyro.sample("triu_star", dist.Bernoulli(logits=mu_net)
+                                       # , infer={"enumerate": "sequential"})
+                                       , infer={"enumerate": "parallel"})
+            # logit_misspec = triu_star*gamma[0] + (1 - triu_star)*gamma[1]
+            # logit_misspec = triu_star*gamma[0] + (1 - triu_star)*(gamma[1] + gamma[2]*nu_diff_norm_val)
+            logit_misspec = jnp.where(triu_star,
+                                      gamma[0],
+                                      gamma[1] + gamma[2] * nu_diff_norm_val)
             numpyro.sample("obs_triu", dist.Bernoulli(logits=logit_misspec), obs=triu_v)
 
     else: # triu_v.ndim == 2
-        with numpyro.plate("gamma_A1", 2):
+        with numpyro.plate("gamma_A1", 3):
+        # with numpyro.plate("gamma_A1", 2):
             gamma_a1 = numpyro.sample("gamma_1", dist.Normal(0, 5))
 
-        with numpyro.plate("gamma_A2", 4):
+        with numpyro.plate("gamma_A2", 5):
+        # with numpyro.plate("gamma_A2", 4):
             gamma_a2 = numpyro.sample("gamma_2", dist.Normal(0, 5))
 
         with ((numpyro.plate("A* and A", x_eq.shape[0]))):
             triu_star = numpyro.sample("triu_star", dist.Bernoulli(logits=mu_net),
                                        infer={"enumerate": "parallel"})
-            logit_misspec_A1 = triu_star*gamma_a1[0] + (1 - triu_star)*gamma_a1[1]
-            logit_misspec_A2 = triu_star*(gamma_a2[0]*(1 - triu_v[0,:]) + gamma_a2[1]*triu_v[0,:])
-            + (1 - triu_star)*(gamma_a2[2]*(1 - triu_v[0,:]) + gamma_a2[3]*triu_v[0,:])
+            # logit_misspec_A1 = triu_star*gamma_a1[0] + (1 - triu_star)*gamma_a1[1]
+            # logit_misspec_A1 = triu_star*gamma_a1[0] + (1 - triu_star)*(gamma_a1[1] + gamma_a1[2]*nu_diff_norm_val)
+            logit_misspec_A1 = jnp.where(triu_star,
+                                         gamma_a1[0],
+                                         gamma_a1[1] + gamma_a1[2] * nu_diff_norm_val)
+            # logit_misspec_A2 = triu_star*(gamma_a2[0] + gamma_a2[1]*triu_v[0,:])
+            # + (1 - triu_star)*(gamma_a2[2] + gamma_a2[3]*triu_v[0,:] + gamma_a2[4]*nu_diff_norm_val)
+            logit_misspec_A2 = jnp.where(triu_star,
+                                         gamma_a2[0] + gamma_a2[1]*triu_v[0,:],
+                                         gamma_a2[2] + gamma_a2[3]*triu_v[0,:] + gamma_a2[4]*nu_diff_norm_val)
+            # logit_misspec_A2 = triu_star*(gamma_a2[0]*(1 - triu_v[0,:]) + gamma_a2[1]*triu_v[0,:])
+            # + (1 - triu_star)*((gamma_a2[2] + gamma_a2[3]*nu_diff_norm_val)*(1 - triu_v[0,:]) + (gamma_a2[4] + gamma_a2[5]*nu_diff_norm_val)*triu_v[0,:])
+            # + (1 - triu_star)*(gamma_a2[2]*(1 - triu_v[0,:]) + gamma_a2[3]*triu_v[0,:])
 
             numpyro.sample("obs_triu_A1", dist.Bernoulli(logits=logit_misspec_A1), obs=triu_v[0,:])
             numpyro.sample("obs_triu_A2", dist.Bernoulli(logits=logit_misspec_A2), obs=triu_v[1,:])
--- Data/Palluck_et_al/util_functions.py
+++ Data/Palluck_et_al/util_functions.py
@@ -8,7 +8,8 @@ import numpyro.distributions as dist
 import numpyro
 from numpyro.contrib.funsor import config_enumerate
 # import
-from numpyro.infer import MCMC, NUTS, Predictive
+from numpyro.infer import MCMC, NUTS, Predictive, SVI, TraceEnum_ELBO, TraceGraph_ELBO
+from numpyro.infer.autoguide import AutoNormal
 from hsgp.approximation import hsgp_squared_exponential, eigenfunctions
 from hsgp.spectral_densities import diag_spectral_density_squared_exponential
 from numpyro_models import noisy_networks_model
@@ -20,7 +21,7 @@ def n_edges_to_n_nodes(n_edges):
 
 class Network_MCMC:
     # def __init__(self, data, rng_key, n_warmup=1000, n_samples=1500, n_chains=4):
-    def __init__(self, network_model, x_eq, triu_v, rng_key, n_warmup=2000, n_samples=4000, n_chains=4, K=2):
+    def __init__(self, network_model, x_eq, triu_v, rng_key, n_warmup=2000, n_samples=4000, n_chains=4, K=2, MCMC = True):
     # def __init__(self, data, n, rng_key, n_warmup=1000, n_samples=2000, n_chains=6):
         self.network_model = network_model
         self.x_eq = x_eq
@@ -32,39 +33,74 @@ class Network_MCMC:
         self.n_samples = n_samples
         self.n_chains = n_chains
         self.K = K
+        self.MCMC = MCMC
         self.network_m = self.network()
         self.post_samples = None
+        self.guide = None
 
     def network(self):
-        kernel = NUTS(self.network_model,
-                      target_accept_prob=0.9,
-                      init_strategy=numpyro.infer.init_to_median(num_samples=20)
-                      )
-        return MCMC(kernel, num_warmup=self.n_warmup, num_samples=self.n_samples,
-                    num_chains=self.n_chains, progress_bar=True)
-                    # num_chains=self.n_chains, progress_bar=False)
+        if self.MCMC:
+            kernel = NUTS(self.network_model,
+                          target_accept_prob=0.9,
+                          init_strategy=numpyro.infer.init_to_median(num_samples=20)
+                          )
+            return MCMC(kernel, num_warmup=self.n_warmup, num_samples=self.n_samples,
+                        num_chains=self.n_chains, progress_bar=True)
+                        # num_chains=self.n_chains, progress_bar=False)
+        else:
+            # self.guide = self.guide()
+            self.guide = AutoNormal(numpyro.handlers.block(self.network_model, hide=["triu_star"]))
+            # self.guide = AutoNormal(self.network_model)
+            # self.guide = AutoNormal(numpyro.handlers.substitute(noisy_networks_model, lambda site: None if site["name"] == "triu_star" else site["value"]))
+            # self.guide = numpyro.infer.autoguide.AutoGuide(noisy_networks_model)
+            # svi = SVI(self.network_model, self.guide, numpyro.optim.Adam(step_size=0.005), TraceEnum_ELBO())
+            svi = SVI(self.network_model, self.guide, numpyro.optim.Adam(step_size=0.005), TraceEnum_ELBO())
+            # svi = SVI(self.network_model, self.guide, numpyro.optim.Adam(step_size=0.005), TraceGraph_ELBO())
+            return svi
+
+    # def guide(self):
+    #     # We use `handlers.substitute` to avoid sampling 'triu_star' in the guide
+    #     with numpyro.handlers.substitute(numpyro.handlers.block(self.network_model, hide=["triu_star"])):
+    #         return AutoNormal(self.network_model)(self.x_eq, self.triu_v, self.N, self.K)
+
+    def run_svi(self):
+        # svi_state = self.network_m.init(self.rng_key, self.x_eq, self.triu_v, self.N, self.K)
+        # num_steps = 10000
+        # for step in range(num_steps):
+        #     svi_state, loss = self.network_m.update(svi_state, self.x_eq, self.triu_v, self.N, self.K)
+        #     if step % 100 == 0:
+        #         print(f'Step {step} Loss: {loss}')
+        # return self.network_m.get_params(svi_state)
+        svi_results = self.network_m.run(self.rng_key,num_steps=10000,
+                                         x_eq=self.x_eq, triu_v=self.triu_v, N=self.N, K=self.K)
+        return svi_results.params
 
     def get_posterior_samples(self):
-        self.network_m.run(self.rng_key,
-                           x_eq=self.x_eq,
-                           triu_v=self.triu_v,
-                           # N_edges = self.N_edges,
-                           N = self.N,
-                           K = self.K)
-        self.network_m.print_summary()
-        self.post_samples = self.network_m.get_samples()
-        return self.post_samples
+        if self.MCMC:
+            self.network_m.run(self.rng_key,
+                               x_eq=self.x_eq,
+                               triu_v=self.triu_v,
+                               # N_edges = self.N_edges,
+                               N = self.N,
+                               K = self.K)
+            self.network_m.print_summary()
+            self.post_samples = self.network_m.get_samples()
+            return self.post_samples
+        else:
+            self.post_samples = self.run_svi()
+            return self.post_samples
 
     def predictive_samples(self):
-        posterior_predictive = Predictive(model=self.network_model,
-                                          posterior_samples=self.post_samples,
-                                          infer_discrete=True)
-        A_star_pred = posterior_predictive(self.rng_key,
-                                          x_eq=self.x_eq,
-                                          triu_v=self.triu_v,
-                                          K = self.K)["triu_star"]
-        nu_pred = posterior_predictive(self.rng_key,
-                                          x_eq=self.x_eq,
-                                          triu_v=self.triu_v,
-                                          K = self.K)["nu"]
-        return A_star_pred, nu_pred
+        if self.MCMC:
+            posterior_predictive = Predictive(model=self.network_model,
+                                              posterior_samples=self.post_samples,
+                                              infer_discrete=True)
+
+            return posterior_predictive(self.rng_key,
+                                        x_eq=self.x_eq,
+                                        triu_v=self.triu_v,
+                                        N=self.N,
+                                        K=self.K)
+        else:
+            predictive = Predictive(self.guide, params=self.post_samples, num_samples=1000)
+            return predictive(self.rng_key, x_eq=self.x_eq, triu_v=self.triu_v, N=self.N, K=self.K)
