ce92b5fd5901cae6aed239f90e118f85a0ff9d35
barwein <barwein@mail.tau.ac.il>
2024-11-28 14:32:41 +0200

    sim file

--- Simulations/linear_dgp_simulations.py
+++ Simulations/linear_dgp_simulations.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 import time
 import numpy as np
 import src.Aux_functions as aux
-from Simulations import results_to_pd_df, one_simuation_iter
+from Simulations.Simulations import results_to_pd_df, one_simuation_iter
 
 
 # parameters guides:
@@ -20,8 +20,8 @@ rng = np.random.default_rng(RANDOM_SEED)
 # parameters
 # THETA = [-1.5, 1.5]
 THETA = [-2, 1.5]
-GAMMA = [1.1, 0.2,  -1, 1]
-GAMMA_REP = [1 , 0.7, -1, 0.6]
+GAMMA = [1.1, 0.2, -1, 1]
+GAMMA_REP = [1, 0.7, -1, 0.6]
 # ETA = jnp.array([-1, 3, -0.25, 0, 3])
 ETA = jnp.array([-1, 3, -0.25, 3])
 SIG_Y = 1.5
@@ -29,7 +29,7 @@ SIG_Y = 1.5
 RHO = 0.9
 PZ = 0.5
 LIN_Y = True
-ALPHAS = (0.7,0.3)
+ALPHAS = (0.7, 0.3)
 N = 500
 # N = 100
 # N_SIM = 500
@@ -46,33 +46,32 @@ if __name__ == "__main__":
     print("N jax cpu devices: ", jax.local_device_count())
 
     # Get fixed data across all simulations
-    fixed_df = aux.GenerateFixedData(rng=rng, theta=THETA, eta=ETA,
-                                     sig_y=SIG_Y, lin=LIN_Y, alphas=ALPHAS, n=N).get_data()
+    fixed_df = aux.GenerateFixedData(
+        rng=rng, theta=THETA, eta=ETA, sig_y=SIG_Y, lin=LIN_Y, alphas=ALPHAS, n=N
+    ).get_data()
 
     start = time.time()
     idx_range = jnp.arange(N_SIM)
 
     # Run simulations
     for i in range(N_SIM):
-        sim_results = one_simuation_iter(idx=i, fixed_df=fixed_df, gamma=GAMMA, gamma_rep=GAMMA_REP,
-                                         eta=ETA, sig_y=SIG_Y, rho=RHO,
-                                         pz=PZ, n_rep=N_REP, lin_y=LIN_Y)
+        sim_results = one_simuation_iter(
+            idx=i,
+            fixed_df=fixed_df,
+            gamma=GAMMA,
+            gamma_rep=GAMMA_REP,
+            eta=ETA,
+            sig_y=SIG_Y,
+            rho=RHO,
+            pz=PZ,
+            n_rep=N_REP,
+            lin_y=LIN_Y,
+        )
         df_results = results_to_pd_df(sim_results, 1)
         w_path = "results"
         dgp = "linear_dgp_test"
         with_header = i == 0
-        df_results.to_csv(w_path + "/" + dgp + ".csv", index=False, mode='a', header=with_header)
-    print("Elapsed time: ", time.time()-start)
-
-
-
-
-
-
-
-
-
-
-
-
-
+        df_results.to_csv(
+            w_path + "/" + dgp + ".csv", index=False, mode="a", header=with_header
+        )
+    print("Elapsed time: ", time.time() - start)
